7e5a761f43eddd91123555a0923076c3988fcbe3
dommaraju Lohith Varma <lxd60600@ucmo.edu>
2024-11-11 22:17:51 -0600

    first commit

---
+++ knife.rb
@@ -0,0 +1,8 @@
+log_level                :info
+log_location             STDOUT
+node_name                "admin"
+client_key               "C:/opscode/chef-workstation/chef/admin.pem"
+chef_server_url          "https://ec2-18-117-142-129.us-east-2.compute.amazonaws.com/organizations/myorg"
+cookbook_path            ["C:/opscode/chef-workstation/chef"]
+ssl_verify_mode          :verify_none
+no_lazy_load             true
